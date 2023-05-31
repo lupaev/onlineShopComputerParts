b7ff85d936689e8401ee7925ddb319619fd62716
sergej <lupaevsv@gmail.com>
2023-05-31 23:43:37 +0700

    Initial commit

---
+++ src/main/java/com/example/onlineshopcomputerparts/OnlineShopComputerPartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopcomputerparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopComputerPartsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineShopComputerPartsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/onlineshopcomputerparts/OnlineShopComputerPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopcomputerparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopComputerPartsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
