af8a3deeb24682083195c1c3bcb9882795219a04
sergej <lupaevsv@gmail.com>
2023-06-03 00:53:08 +0700

    mapper tests

--- src/test/java/com/example/onlineshopcomputerparts/Mapper/ComputerMapperTest.java
+++ src/test/java/com/example/onlineshopcomputerparts/Mapper/ComputerMapperTest.java
@@ -1,52 +1,132 @@
 package com.example.onlineshopcomputerparts.Mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.example.onlineshopcomputerparts.DTO.ComputerDTO;
 import com.example.onlineshopcomputerparts.Entity.Computer;
+import java.util.Collection;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ComputerMapperTest {
 
   private ComputerMapper computerMapper = new ComputerMapperImpl();
 
-  @Test
-  void given_customer_entity_mapper_should_map_all_fields_to_customer_dto() {
-    Computer computer = new Computer();
+  private ComputerDTO computerDTO;
+
+  private Computer computer;
+
+  @BeforeEach
+  void setUp() {
+    computerDTO = new ComputerDTO(2L, 1111, "Test1", 12601.98,
+        11, "mini1");
+    computer = new Computer();
     computer.setId(1L);
-    computer.setPrice(10);
-    computer.setManufacturer("ivi");
-    computer.setForm("d");
-    computer.setQuantity(100);
-    computer.setSerialNumber(101);
-    ComputerDTO computerDTO = computerMapper.toDTO(computer);
-    assertThat(computerDTO.getId()).isEqualTo(1L);
-    assertThat(computerDTO.getPrice()).isEqualTo(10);
-    assertThat(computerDTO.getManufacturer()).isEqualTo("ivi");
-    assertThat(computerDTO.getForm()).isEqualTo("d");
-    assertThat(computerDTO.getQuantity()).isEqualTo(100);
-    assertThat(computerDTO.getSerialNumber()).isEqualTo(101);
+    computer.setPrice(12600.98);
+    computer.setManufacturer("Test");
+    computer.setForm("mini");
+    computer.setQuantity(1);
+    computer.setSerialNumber(111);
 
+  }
+
+  @AfterEach
+  void afterEach() {
+    computer = null;
+    computerDTO = null;
   }
 
   @Test
-  void given_customer_dto_mapper_should_map_all_fields_to_customer_entity() {
-    ComputerDTO computerDTO = new ComputerDTO();
-    computerDTO.setId(1L);
-    computerDTO.setPrice(10);
-    computerDTO.setManufacturer("ivi");
-    computerDTO.setForm("d");
-    computerDTO.setQuantity(100);
-    computerDTO.setSerialNumber(101);
-    Computer computer = computerMapper.toEntity(computerDTO);
-    assertThat(computer.getId()).isEqualTo(1L);
-    assertThat(computer.getPrice()).isEqualTo(10);
-    assertThat(computer.getManufacturer()).isEqualTo("ivi");
-    assertThat(computer.getForm()).isEqualTo("d");
-    assertThat(computer.getQuantity()).isEqualTo(100);
-    assertThat(computer.getSerialNumber()).isEqualTo(101);
+  void toDTOPositiv() {
+    assertNotNull(computer);
+    assertNotNull(computerDTO);
+    ComputerDTO dto = computerMapper.toDTO(computer);
+    assertNotNull(dto);
+    assertThat(dto.getId()).isEqualTo(computer.getId());
+    assertThat(dto.getPrice()).isEqualTo(computer.getPrice());
+    assertThat(dto.getManufacturer()).isEqualTo(computer.getManufacturer());
+    assertThat(dto.getForm()).isEqualTo(computer.getForm());
+    assertThat(dto.getQuantity()).isEqualTo(computer.getQuantity());
+    assertThat(dto.getSerialNumber()).isEqualTo(computer.getSerialNumber());
   }
 
+  @Test
+  void toDTONegativ() {
+    assertNotNull(computer);
+    assertNotNull(computerDTO);
+    ComputerDTO dto = computerMapper.toDTO(computer);
+    assertNotNull(dto);
+    assertNotEquals(dto.getId(), computerDTO.getId());
+    assertNotEquals(dto.getPrice(), computerDTO.getPrice());
+    assertNotEquals(dto.getManufacturer(), computerDTO.getManufacturer());
+    assertNotEquals(dto.getForm(), computerDTO.getForm());
+    assertNotEquals(dto.getQuantity(), computerDTO.getQuantity());
+    assertNotEquals(dto.getSerialNumber(), computerDTO.getSerialNumber());
+  }
+
+  @Test
+  void toEntityPositiv() {
+    assertNotNull(computer);
+    assertNotNull(computerDTO);
+    Computer comp = computerMapper.toEntity(computerDTO);
+    assertNotNull(comp);
+    assertThat(comp.getId()).isEqualTo(computerDTO.getId());
+    assertThat(comp.getPrice()).isEqualTo(computerDTO.getPrice());
+    assertThat(comp.getManufacturer()).isEqualTo(computerDTO.getManufacturer());
+    assertThat(comp.getForm()).isEqualTo(computerDTO.getForm());
+    assertThat(comp.getQuantity()).isEqualTo(computerDTO.getQuantity());
+    assertThat(comp.getSerialNumber()).isEqualTo(computerDTO.getSerialNumber());
+  }
+
+  @Test
+  void toEntityNegativ() {
+    assertNotNull(computer);
+    assertNotNull(computerDTO);
+    Computer comp = computerMapper.toEntity(computerDTO);
+    assertNotNull(comp);
+    assertNotEquals(comp.getId(), computer.getId());
+    assertNotEquals(comp.getPrice(), computer.getPrice());
+    assertNotEquals(comp.getManufacturer(), computer.getManufacturer());
+    assertNotEquals(comp.getForm(), computer.getForm());
+    assertNotEquals(comp.getQuantity(), computer.getQuantity());
+    assertNotEquals(comp.getSerialNumber(), computer.getSerialNumber());
+  }
+
+  @Test
+  void toListDTOPositiv() {
+    assertNotNull(computer);
+    Collection<ComputerDTO> dto = computerMapper.toDTOList(List.of(computer));
+    assertNotNull(dto);
+    assertTrue(dto.contains(computerMapper.toDTO(computer)));
+  }
+
+  @Test
+  void toListDTONegativ() {
+    assertNotNull(computer);
+    Collection<ComputerDTO> dto = computerMapper.toDTOList(List.of(computer));
+    assertNotNull(dto);
+    assertTrue(dto.contains(computerMapper.toDTO(computer)));
+  }
+
+  @Test
+  void updateComputerFromDto() {
+    assertNotNull(computerDTO);
+    assertNotNull(computer);
+    computerMapper.updateComputerFromDto(computerDTO, computer);
+    assertEquals(computer, computerMapper.toEntity(computerDTO));
+    Computer c = new Computer();
+    c.setId(1L);
+    c.setPrice(12600.98);
+    c.setManufacturer("Test");
+    c.setForm("mini");
+    c.setQuantity(1);
+    c.setSerialNumber(111);
+    assertNotEquals(computer, c);
+   }
 
 }
---
+++ src/test/java/com/example/onlineshopcomputerparts/Mapper/MonitorMapperTest.java
@@ -0,0 +1,130 @@
+package com.example.onlineshopcomputerparts.Mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.onlineshopcomputerparts.DTO.LaptopDTO;
+import com.example.onlineshopcomputerparts.DTO.MonitorDTO;
+import com.example.onlineshopcomputerparts.Entity.Laptop;
+import com.example.onlineshopcomputerparts.Entity.Monitor;
+import java.util.Collection;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MonitorMapperTest {
+
+  private MonitorMapper monitorMapper = new MonitorMapperImpl();
+
+  private MonitorDTO monitorDTO;
+
+  private Monitor monitor;
+
+  @BeforeEach
+  void setUp() {
+    monitorDTO = new MonitorDTO(2L, 1111, "Test1", 12601.98,
+        11, 17);
+    monitor= new Monitor();
+    monitor.setId(1L);
+    monitor.setPrice(12600.98);
+    monitor.setManufacturer("Test");
+    monitor.setDiagonal(24);
+    monitor.setQuantity(1);
+    monitor.setSerialNumber(111);
+  }
+
+  @AfterEach
+  void afterEach() {
+    monitor = null;
+    monitorDTO = null;
+  }
+
+  @Test
+  void toDTOPositiv() {
+    assertNotNull(monitor);
+    assertNotNull(monitorDTO);
+    MonitorDTO dto = monitorMapper.toDTO(monitor);
+    assertNotNull(dto);
+    assertThat(dto.getId()).isEqualTo(monitor.getId());
+    assertThat(dto.getPrice()).isEqualTo(monitor.getPrice());
+    assertThat(dto.getManufacturer()).isEqualTo(monitor.getManufacturer());
+    assertThat(dto.getDiagonal()).isEqualTo(monitor.getDiagonal());
+    assertThat(dto.getQuantity()).isEqualTo(monitor.getQuantity());
+    assertThat(dto.getSerialNumber()).isEqualTo(monitor.getSerialNumber());
+  }
+
+  @Test
+  void toDTONegativ() {
+    assertNotNull(monitor);
+    assertNotNull(monitorDTO);
+    MonitorDTO dto = monitorMapper.toDTO(monitor);
+    assertNotNull(dto);
+    assertNotEquals(dto.getId(), monitorDTO.getId());
+    assertNotEquals(dto.getPrice(), monitorDTO.getPrice());
+    assertNotEquals(dto.getManufacturer(), monitorDTO.getManufacturer());
+    assertNotEquals(dto.getDiagonal(), monitorDTO.getDiagonal());
+    assertNotEquals(dto.getQuantity(), monitorDTO.getQuantity());
+    assertNotEquals(dto.getSerialNumber(), monitorDTO.getSerialNumber());
+  }
+
+  @Test
+  void toEntityPositiv() {
+    assertNotNull(monitor);
+    assertNotNull(monitorDTO);
+    Monitor l = monitorMapper.toEntity(monitorDTO);
+    assertNotNull(l);
+    assertThat(l.getId()).isEqualTo(monitorDTO.getId());
+    assertThat(l.getPrice()).isEqualTo(monitorDTO.getPrice());
+    assertThat(l.getManufacturer()).isEqualTo(monitorDTO.getManufacturer());
+    assertThat(l.getDiagonal()).isEqualTo(monitorDTO.getDiagonal());
+    assertThat(l.getQuantity()).isEqualTo(monitorDTO.getQuantity());
+    assertThat(l.getSerialNumber()).isEqualTo(monitorDTO.getSerialNumber());
+  }
+
+  @Test
+  void toEntityNegativ() {
+    assertNotNull(monitor);
+    assertNotNull(monitorDTO);
+    Monitor l = monitorMapper.toEntity(monitorDTO);
+    assertNotNull(l);
+    assertNotEquals(l.getId(), monitor.getId());
+    assertNotEquals(l.getPrice(), monitor.getPrice());
+    assertNotEquals(l.getManufacturer(), monitor.getManufacturer());
+    assertNotEquals(l.getDiagonal(), monitor.getDiagonal());
+    assertNotEquals(l.getQuantity(), monitor.getQuantity());
+    assertNotEquals(l.getSerialNumber(), monitor.getSerialNumber());
+  }
+
+  @Test
+  void toListDTOPositiv() {
+    assertNotNull(monitor);
+    Collection<MonitorDTO> dto = monitorMapper.toDTOList(List.of(monitor));
+    assertNotNull(dto);
+    assertTrue(dto.contains(monitorMapper.toDTO(monitor)));
+  }
+
+  @Test
+  void toListDTONegativ() {
+    assertNotNull(monitor);
+    Collection<MonitorDTO> dto = monitorMapper.toDTOList(List.of(monitor));
+    assertNotNull(dto);
+    assertTrue(dto.contains(monitorMapper.toDTO(monitor)));
+  }
+
+  @Test
+  void updateComputerFromDto() {
+    assertNotNull(monitorDTO);
+    assertNotNull(monitor);
+    monitorMapper.updateMonitorFromDto(monitorDTO, monitor);
+    assertEquals(monitor, monitorMapper.toEntity(monitorDTO));
+    Monitor c = new Monitor();
+    c.setId(1L);
+    c.setPrice(12600.98);
+    c.setManufacturer("Test");
+    c.setDiagonal(6);
+    c.setQuantity(1);
+    c.setSerialNumber(111);
+    assertNotEquals(monitor, c);
+  }
+}
