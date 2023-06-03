6a3cb79d7f8321fc76635d7ff00d1af1ec5b1345
sergej <lupaevsv@gmail.com>
2023-06-03 16:19:44 +0700

    service tests

--- src/test/java/com/example/onlineshopcomputerparts/Service/HddServiceTest.java
+++ src/test/java/com/example/onlineshopcomputerparts/Service/HddServiceTest.java
@@ -15,12 +15,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.example.onlineshopcomputerparts.DTO.ComputerDTO;
-import com.example.onlineshopcomputerparts.Entity.Computer;
+import com.example.onlineshopcomputerparts.DTO.HddDTO;
+import com.example.onlineshopcomputerparts.Entity.Hdd;
 import com.example.onlineshopcomputerparts.Exception.ElemNotFound;
-import com.example.onlineshopcomputerparts.Mapper.ComputerMapper;
-import com.example.onlineshopcomputerparts.Repository.ComputerRepository;
-import com.example.onlineshopcomputerparts.Service.impl.ComputerServiceImpl;
+import com.example.onlineshopcomputerparts.Mapper.HddMapper;
+import com.example.onlineshopcomputerparts.Repository.HddRepository;
+import com.example.onlineshopcomputerparts.Service.impl.HddServiceImpl;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
@@ -33,65 +33,66 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class ComputerServiceTest {
+class HddServiceTest {
+
 
   @InjectMocks
-  private ComputerServiceImpl service;
+  private HddServiceImpl service;
   @Mock
-  private ComputerRepository repository;
+  private HddRepository repository;
   @Spy
-  private ComputerMapper mapper;
+  private HddMapper mapper;
 
-  private ComputerDTO dto;
-  private Computer computer;
+  private HddDTO dto;
+  private Hdd hdd;
 
   @BeforeEach
   void setUp() {
-    dto = new ComputerDTO(2L, 1111, "Test1", 12601.98,
-        11, "mini1");
-    computer = new Computer();
-    computer.setId(dto.getId());
-    computer.setPrice(dto.getPrice());
-    computer.setManufacturer(dto.getManufacturer());
-    computer.setForm(dto.getForm());
-    computer.setQuantity(dto.getQuantity());
-    computer.setSerialNumber(dto.getSerialNumber());
+    dto = new HddDTO(2L, 1111, "Test1", 12601.98,
+        11, 10);
+    hdd = new Hdd();
+    hdd.setId(dto.getId());
+    hdd.setPrice(dto.getPrice());
+    hdd.setManufacturer(dto.getManufacturer());
+    hdd.setVolumeGb(dto.getVolumeGb());
+    hdd.setQuantity(dto.getQuantity());
+    hdd.setSerialNumber(dto.getSerialNumber());
   }
 
   @AfterEach
   void clearTestData() {
     dto = null;
-    computer = null;
+    hdd = null;
   }
 
   @Test
   void addPositiveTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
-    when(mapper.toEntity(dto)).thenReturn(computer);
+    when(mapper.toEntity(dto)).thenReturn(hdd);
     when(service.add(dto)).thenReturn(dto);
-    when(repository.save(any(Computer.class))).thenReturn(computer);
-    assertThat(service.add(dto)).isNotNull().isEqualTo(dto).isExactlyInstanceOf(ComputerDTO.class);
-    assertThat(mapper.toEntity(dto)).isNotNull().isEqualTo(computer)
-        .isExactlyInstanceOf(Computer.class);
-    assertThat(repository.save(computer)).isNotNull().isExactlyInstanceOf(Computer.class);
+    when(repository.save(any(Hdd.class))).thenReturn(hdd);
+    assertThat(service.add(dto)).isNotNull().isEqualTo(dto).isExactlyInstanceOf(HddDTO.class);
+    assertThat(mapper.toEntity(dto)).isNotNull().isEqualTo(hdd)
+        .isExactlyInstanceOf(Hdd.class);
+    assertThat(repository.save(hdd)).isNotNull().isExactlyInstanceOf(Hdd.class);
 
-    verify(repository, times(1)).save(computer);
+    verify(repository, times(1)).save(hdd);
     verify(service, times(1)).add(dto);
     verify(mapper, times(1)).toEntity(dto);
   }
 
   @Test
   void addNegativeTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
     when(mapper.toEntity(any())).thenThrow(NullPointerException.class);
     when(repository.save(any())).thenThrow(RuntimeException.class);
     when(service.add(any())).thenThrow(RuntimeException.class);
     assertThrows(NullPointerException.class, () -> mapper.toEntity(any()));
-    assertThrows(RuntimeException.class, () -> repository.save(any(Computer.class)));
+    assertThrows(RuntimeException.class, () -> repository.save(any(Hdd.class)));
     assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> service.add(dto));
 
     verify(repository, times(1)).save(any());
@@ -101,87 +102,87 @@ class ComputerServiceTest {
 
   @Test
   void patchPositiveTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
-    when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(computer));
-    when(mapper.toDTO(computer)).thenReturn(dto);
-    doNothing().when(mapper).updateComputerFromDto(dto, computer);
-    when(repository.save(any(Computer.class))).thenReturn(computer);
-    when(service.patch(dto.getId(), dto.getForm(), dto.getSerialNumber(), dto.getManufacturer(),
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
+    when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(hdd));
+    when(mapper.toDTO(hdd)).thenReturn(dto);
+    doNothing().when(mapper).updateHddFromDto(dto, hdd);
+    when(repository.save(any(Hdd.class))).thenReturn(hdd);
+    when(service.patch(dto.getId(), dto.getVolumeGb(), dto.getSerialNumber(), dto.getManufacturer(),
         dto.getPrice(), dto.getQuantity())).thenReturn(dto);
 
     assertThat(repository.findById(anyLong())).isNotNull();
-    assertThat(mapper.toDTO(computer)).isNotNull().isEqualTo(dto)
-        .isExactlyInstanceOf(ComputerDTO.class);
+    assertThat(mapper.toDTO(hdd)).isNotNull().isEqualTo(dto)
+        .isExactlyInstanceOf(HddDTO.class);
     assertDoesNotThrow(
-        () -> mapper.updateComputerFromDto(dto, computer));
-    assertThat(repository.save(computer)).isNotNull().isExactlyInstanceOf(Computer.class);
+        () -> mapper.updateHddFromDto(dto, hdd));
+    assertThat(repository.save(hdd)).isNotNull().isExactlyInstanceOf(Hdd.class);
 
     assertThat(
-        service.patch(dto.getId(), dto.getForm(), dto.getSerialNumber(), dto.getManufacturer(),
+        service.patch(dto.getId(), dto.getVolumeGb(), dto.getSerialNumber(), dto.getManufacturer(),
             dto.getPrice(), dto.getQuantity())).isNotNull().isEqualTo(dto)
-        .isExactlyInstanceOf(ComputerDTO.class);
+        .isExactlyInstanceOf(HddDTO.class);
 
-    verify(repository, times(1)).save(computer);
+    verify(repository, times(1)).save(hdd);
     verify(repository, times(1)).findById(anyLong());
-    verify(mapper, times(1)).toDTO(any(Computer.class));
-    verify(mapper, times(1)).updateComputerFromDto(any(ComputerDTO.class), any(Computer.class));
+    verify(mapper, times(1)).toDTO(any(Hdd.class));
+    verify(mapper, times(1)).updateHddFromDto(any(HddDTO.class), any(Hdd.class));
 
-    verify(service, times(1)).patch(anyLong(), anyString(), anyInt(),
+    verify(service, times(1)).patch(anyLong(), anyInt(), anyInt(),
         anyString(), anyDouble(), anyInt());
 
   }
 
   @Test
   void patchNegativeTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
     when(repository.findById(anyLong())).thenThrow(ElemNotFound.class);
     when(mapper.toDTO(any())).thenThrow(NullPointerException.class);
-    doThrow(NullPointerException.class).when(mapper).updateComputerFromDto(any(), any());
+    doThrow(NullPointerException.class).when(mapper).updateHddFromDto(any(), any());
     when(repository.save(any())).thenThrow(RuntimeException.class);
-    when(service.patch(dto.getId(), dto.getForm(), dto.getSerialNumber(), dto.getManufacturer(),
+    when(service.patch(dto.getId(), dto.getVolumeGb(), dto.getSerialNumber(), dto.getManufacturer(),
         dto.getPrice(), dto.getQuantity())).thenThrow(RuntimeException.class);
     assertThrows(ElemNotFound.class, () -> repository.findById(anyLong()));
     assertThrows(NullPointerException.class, () -> mapper.toDTO(any()));
-    assertThrows(NullPointerException.class, () -> mapper.updateComputerFromDto(any(), any()));
+    assertThrows(NullPointerException.class, () -> mapper.updateHddFromDto(any(), any()));
     assertThrows(RuntimeException.class, () -> repository.save(any()));
     assertThatExceptionOfType(RuntimeException.class).isThrownBy(
-        () -> service.patch(dto.getId(), dto.getForm(), dto.getSerialNumber(),
+        () -> service.patch(dto.getId(), dto.getVolumeGb(), dto.getSerialNumber(),
             dto.getManufacturer(), dto.getPrice(), dto.getQuantity()));
 
     verify(repository, times(1)).save(any());
     verify(repository, times(1)).findById(anyLong());
     verify(mapper, times(1)).toDTO(any());
-    verify(mapper, times(1)).updateComputerFromDto(any(), any());
+    verify(mapper, times(1)).updateHddFromDto(any(), any());
 
-    verify(service, times(1)).patch(anyLong(), anyString(), anyInt(), anyString(), anyDouble(),
+    verify(service, times(1)).patch(anyLong(), anyInt(), anyInt(), anyString(), anyDouble(),
         anyInt());
 
   }
 
   @Test
   void findAllPositiveTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
-    when(repository.findAll()).thenReturn(List.of(computer));
-    when(mapper.toDTOList(List.of(computer))).thenReturn(List.of(dto));
+    when(repository.findAll()).thenReturn(List.of(hdd));
+    when(mapper.toDTOList(List.of(hdd))).thenReturn(List.of(dto));
     when(service.findAll()).thenReturn(List.of(dto));
 
-    assertThat(repository.findAll()).isNotNull().isEqualTo(List.of(computer));
-    assertThat(mapper.toDTOList(List.of(computer))).isNotNull().isEqualTo(List.of(dto));
+    assertThat(repository.findAll()).isNotNull().isEqualTo(List.of(hdd));
+    assertThat(mapper.toDTOList(List.of(hdd))).isNotNull().isEqualTo(List.of(dto));
     assertThat(service.findAll()).isNotNull().isEqualTo(List.of(dto));
 
     verify(repository, times(1)).findAll();
     verify(service, times(1)).findAll();
-    verify(mapper, times(1)).toDTOList(List.of(computer));
+    verify(mapper, times(1)).toDTOList(List.of(hdd));
   }
 
   @Test
   void findAllNegativeTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
 
     when(repository.findAll()).thenThrow(ElemNotFound.class);
     when(service.findAll()).thenThrow(RuntimeException.class);
@@ -195,28 +196,28 @@ class ComputerServiceTest {
 
   @Test
   void findByIdPositiveTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
-    when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(computer));
-    when(mapper.toDTO(computer)).thenReturn(dto);
+    when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(hdd));
+    when(mapper.toDTO(hdd)).thenReturn(dto);
     when(service.findById(anyLong())).thenReturn(dto);
 
     assertThat(repository.findById(anyLong())).isNotNull();
-    assertThat(mapper.toDTO(computer)).isNotNull().isEqualTo(dto)
-        .isExactlyInstanceOf(ComputerDTO.class);
+    assertThat(mapper.toDTO(hdd)).isNotNull().isEqualTo(dto)
+        .isExactlyInstanceOf(HddDTO.class);
     assertThat(service.findById(anyLong())).isNotNull().isEqualTo(dto)
-        .isExactlyInstanceOf(ComputerDTO.class);
+        .isExactlyInstanceOf(HddDTO.class);
 
     verify(repository, times(1)).findById(anyLong());
-    verify(mapper, times(1)).toDTO(any(Computer.class));
+    verify(mapper, times(1)).toDTO(any(Hdd.class));
     verify(service, times(1)).findById(anyLong());
   }
 
   @Test
   void findByIdNegativeTest() {
-    ComputerServiceImpl service = mock(ComputerServiceImpl.class);
-    ComputerMapper mapper = mock(ComputerMapper.class);
+    HddServiceImpl service = mock(HddServiceImpl.class);
+    HddMapper mapper = mock(HddMapper.class);
 
     when(repository.findById(anyLong())).thenThrow(ElemNotFound.class);
     when(mapper.toDTO(any())).thenThrow(NullPointerException.class);
@@ -230,6 +231,5 @@ class ComputerServiceTest {
     verify(mapper, times(1)).toDTO(any());
     verify(service, times(1)).findById(anyLong());
   }
-
 
 }
