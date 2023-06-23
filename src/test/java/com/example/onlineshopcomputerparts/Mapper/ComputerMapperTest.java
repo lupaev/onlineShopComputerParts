22d10fcbe7efbe1b58fbae158ed0b0317a05f723
sergej <lupaevsv@gmail.com>
2023-06-23 15:25:26 +0700

    fix

--- src/test/java/com/example/onlineshopcomputerparts/Mapper/ComputerMapperTest.java
+++ src/test/java/com/example/onlineshopcomputerparts/Mapper/ComputerMapperTest.java
@@ -12,7 +12,9 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ComputerMapperTest {
 
   private ComputerMapper computerMapper = new ComputerMapperImpl();
